b753defa6a711d25ec75ab31fdf1ea81f9a0756a
Nitesh Balusu <niteshbalusu@icloud.com>
2026-02-09 09:05:06 -0500

    Bump nitro-modules to 0.33.7

--- react-native-nitro-ark/nitrogen/generated/ios/NitroArkAutolinking.swift
+++ react-native-nitro-ark/nitrogen/generated/ios/NitroArkAutolinking.swift
@@ -5,6 +5,10 @@
 /// Copyright © Marc Rousavy @ Margelo
 ///
 
+import NitroModules
+
+// TODO: Use empty enums once Swift supports exporting them as namespaces
+//       See: https://github.com/swiftlang/swift/pull/83616
 public final class NitroArkAutolinking {
   public typealias bridge = margelo.nitro.nitroark.bridge.swift
 
